6fdf07ef31fd26cc5e4fc783491b0e883017cff7
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-14 11:29:53 +0900

    Update 1108박종현_023.py

--- 1108박종현_023.py
+++ 1108박종현_023.py
@@ -1,7 +1,7 @@
-print('1:', 10 > 5) # True
-print('2:', 10 < 3) # False
-print('3:', 10 > 5 or 10 < 3) # True
-print('4:', 10 > 5 and 10 < 3) # False
-print('5:', not True) # False
-print('6:', not True and True) # False
-print('7:', not True or True) # True
+print('1:', 10 > 5)  # True
+print('2:', 10 < 3)  # False
+print('3:', 10 > 5 or 10 < 3)  # True
+print('4:', 10 > 5 and 10 < 3)  # False
+print('5:', not True)  # False
+print('6:', not True and True)  # False
+print('7:', not True or True)  # True
