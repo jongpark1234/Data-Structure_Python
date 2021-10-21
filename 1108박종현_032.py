36e93abbd0fe0374112e47969a909e27077b062e
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-21 11:26:52 +0900

    2021_10_21 (2)
    ㆍ팩토리얼
    ㆍ리스트
    ㆍ딕셔너리

--- 1108박종현_032.py
+++ 1108박종현_032.py
@@ -5,3 +5,10 @@ arr = []
 for i in range(6):
     arr.append(randint(1, 46))
 print(arr)
+
+# [문제] arr에 있는 데이터 중 가장 큰 값을 반환하시오.
+print(max(arr))
+
+# [문제] 리스트에서 두번째 큰 값을 출력하시오.
+arr.sort(reverse=True)
+print(arr[1])
