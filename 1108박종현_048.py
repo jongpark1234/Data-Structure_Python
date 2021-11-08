4dcbee427d602e8e6c586b1b4f920660ee6ebcc7
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-11-08 16:10:38 +0900

    2021_11_08 (2)

--- 1108박종현_048.py
+++ 1108박종현_048.py
@@ -1,11 +1,11 @@
 from collections import deque
 def bfs(start):
-    queue = deque([start])
-    visited[start] = True
-    while queue:
-        v = queue.popleft()
-        print(v, end=' ')
-        for i in graph[v]:
+    queue = deque([start]) # 덱 선언
+    visited[start] = True # 정복 여부 True 설정
+    while queue: # 덱이 빌 때 까지 반복함
+        v = queue.popleft() # 덱에서 하나를 빼냄
+        print(v, end=' ') # 출력
+        for i in graph[v]: 
             if not visited[i]:
                 queue.append(i)
                 visited[i] = True
