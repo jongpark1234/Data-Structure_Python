ee1488efc59ca660e14fdab540ae2be493ab64ec
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-11-15 15:52:11 +0900

    2021_11_05 (3)

---
+++ 1108박종현_050.py
@@ -0,0 +1,8 @@
+for i in range(int(input()) + 1):
+	for j in range(60):
+		for k in range(60):
+			try:
+				cnt += str(i).count('3') + str(j).count('3') + str(k).count('3')
+			except NameError:
+				cnt = str(i).count('3') + str(j).count('3') + str(k).count('3')
+print(cnt)
